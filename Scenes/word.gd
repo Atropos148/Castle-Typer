3a25130273c46c3b3fb1caba0856c9928851b443
Atropos148 <tibor.h148@gmail.com>
2022-11-20 15:14:45 +0100

    Added Correct Letter Overlay
    - Word now shows correctly typed letters, and resets if player types a wrong letter

--- Scenes/word.gd
+++ Scenes/word.gd
@@ -6,23 +6,31 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-var assigned_word: String = "castle"
+var assigned_word: String
 
 var next_letter_position: int = 0
 
 @export var word_label: Label
+@export var correct_letters_label: Label
 
 func _ready() -> void:
-	word_label.text = assigned_word
+	set_assigned_word("castle")
 
 func check_pressed_letter(pressed_letter: String):
 	if assigned_word[next_letter_position] == pressed_letter.to_lower():
-		print("correct letter")
+		correct_letters_label.text += assigned_word[next_letter_position]
 		if next_letter_position + 1 < assigned_word.length():
 			next_letter_position += 1
+		else:
+			queue_free()
 	else:
 		print("wrong letter")
 		next_letter_position = 0
+		correct_letters_label.text = ""
+
+func set_assigned_word(word: String):
+	assigned_word = word
+	word_label.text = assigned_word
 
 #func _physics_process(delta: float) -> void:
 #	# Add the gravity.
